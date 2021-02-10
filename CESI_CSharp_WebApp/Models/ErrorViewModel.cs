fac83d38472bd40a6d55bc253129bfc74ca617b6
InFinity_54 <corentinkreicher@gmail.com>
2021-02-10 09:27:57 +0100

    Création du projet

---
+++ CESI_CSharp_WebApp/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace CESI_CSharp_WebApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
